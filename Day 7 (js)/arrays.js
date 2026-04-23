ab534fca3c10195b4a2c2b804ac9b91a7f817272
Akhil Bhojane <akhilbhojne@gmail.com>
2026-04-23 23:11:40 +0530

    day 5 task

---
+++ Day 7 (js)/arrays.js
@@ -0,0 +1,9 @@
+//Arrays
+
+let numbertest = [1, 2, 3, 4, 5];
+
+
+
+for (let i = 0; i < numbertest.length; i++) {
+  console.log(numbertest[i]);
+}
---
+++ Day 7 (js)/script.js
@@ -0,0 +1,30 @@
+//Default function
+
+function yoo(){
+    console.log("how are you?")
+}
+
+yoo()
+
+//Functions with parameter
+
+function addition(a,b){
+    console.log(a+b)
+
+}
+
+addition(5,5)
+
+//Functions with return value
+
+function multiply(a,b){
+    return a+b;
+}
+
+console.log(multiply(5,855))
+
+//Arrow functions
+
+let test = (resting) =>{
+    
+}
